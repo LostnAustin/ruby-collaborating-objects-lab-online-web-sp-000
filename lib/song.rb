4b8cd7ef1c2bc8cfb32d53a8a497c37fd2287bee
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-04 16:54:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,8 @@ end
   #filename.song = self
 end
 
-    def artist_name(artist_name)
+    def artist_name=(artist_name)
+      @artist = artist
 
     artist = Artist.find_or_create_by_name(artist_name)
 
