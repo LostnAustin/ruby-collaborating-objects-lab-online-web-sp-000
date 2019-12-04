770bfbc6a7c85ba9824290b102885fedd8bf9d24
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-04 17:36:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ end
   def self.new_by_filename(filename)
 #binding.pry
   song = Song.new(filename.split(" - ")[1])
-  song.artist_name=(filename[0])
+  song.artist_name=(self)
 end
 
     def artist_name=(artist_name)
